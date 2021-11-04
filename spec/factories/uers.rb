c1246253278062e424efbf118fa4d2881663f0c5
techcamp111miyazaki <kirohi19980715@gmail.com>
2021-11-04 12:14:26 +0900

    test-code 事前準備

---
+++ spec/factories/uers.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name "苗字"
+    first_name "名前"
+    last_name_reading "ミョウジ"
+    first_name_reading "ナマエ"
+    birthday {Faker::Date.between(from: 5.days.ago, to: Date.today)}
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  
   describe "ユーザー新規登録" do
-    
+    context '新規登録できる場合' do
+      it "nickname...birthdayまで全て存在すれば登録できる" do
+        
+      end
+      it "passwordが6文字以上で登録できる" do
+        
+      end
+    end
+    context '新規登録できない場合' do
+      it "nicknameが空だと登録できない" do
+        
+      end
+      it "emailが空だと登録できない" do
+        
+      end
+      it "重複したemailだと保存ができない" do
+        
+      end
+      it "passwordが空だと登録できない" do
+        
+      end
+      it "passwordが5文字以下だと登録できない" do
+        
+      end
+      it "passwordとpassword_confirmationが不一致だと登録できない" do
+        
+      end
+      it "last_nameが空だと登録できない" do
+        
+      end
+      it "first_nameが空だと登録できない" do
+      
+      end
+      it "last_name_readingが空だと登録できない" do
+        
+      end
+      it "first_name_readingが空だと登録できない" do
+        
+      end
+      it "birthdayが空だと登録できない" do
+        
+      end
+    end
   end
 end
