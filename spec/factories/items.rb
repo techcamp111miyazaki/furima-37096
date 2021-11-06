85b16b98b879b59dc46ad4df16f5c35f2139ba77
techcamp111miyazaki <kirohi19980715@gmail.com>
2021-11-06 11:23:32 +0900

    test-codeの洗い出し

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,12 +1,18 @@
 FactoryBot.define do
   factory :item do
-    item_name {}
-    explanation {}
-    category_id {}
-    status_id {}
-    price {}
-    prefecture_id {}
-    shipment_fee_id {}
-    shipment_days_id {}
+    item_name {Faker::Name.name}
+    explanation {Faker::Lorem.sentence}
+    category_id {Faker::Number.between(from: 2, to: 11)}
+    status_id {Faker::Number.between(from: 2, to: 7)}
+    prefecture_id {Faker::Number.between(from: 2, to: 48)}
+    shipment_fee_id {Faker::Number.between(from: 2, to: 3)}
+    shipment_days_id {Faker::Number.between(from: 2, to: 4)}
+    price {Faker::Number.between(from: 300, to: 9999999)}
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/image.png'), filename: 'image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe "商品新規保存" do
+    context "新規保存できる場合" do
+      it "image..priceまで全て存在すれば保存できる" do
+        expect(@item).to be_valid
+      end
+      it "priceが半角表記だと保存できる" do
+        @item.price = "1111"
+        expect(@item).to be_valid
+      end
+    end
+    context "新規保存できない場合" do
+      it "imageが空だと保存できない" do
+        @item.image = nil
+        @item.valid?
+      end
+      it "item_nameが空だと保存できない" do
+        
+      end
+      it "explanationが空だと保存できない" do
+        
+      end
+      it "category_idが1だと保存できない" do
+        
+      end
+      it "status_idが1だと保存できない" do
+        
+      end
+      it "priceが空だと保存できない" do
+        
+      end
+      it "priceが全角表記だと保存できない" do
+        
+      end
+      it "priceが半角と全角の混合表記だと保存できない" do
+        
+      end
+      it "priceが300以下だと保存できない" do
+        
+      end
+      it "priceが10000000以上だと保存できない" do
+        
+      end
+      it "prefecture_idが1だと保存できない" do
+        
+      end
+      it "shipment_fee_idが1だと保存できない" do
+        
+      end
+      it "shipment_days_idが1だと保存できない" do
+        
+      end
+    end
+  end
 end
