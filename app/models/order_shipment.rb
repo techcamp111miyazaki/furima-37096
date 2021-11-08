4c3842b4e179cbcd876c37df12249f81d977378d
techcamp111miyazaki <kirohi19980715@gmail.com>
2021-11-08 17:43:20 +0900

    orderのview&route&migrateの作成

---
+++ app/models/order_shipment.rb
@@ -0,0 +1,4 @@
+class OrderShipment
+  include ActiveModel::Model
+  
+end
--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,2 +1,7 @@
 class Shipment < ApplicationRecord
+  
+  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+  
+  belongs_to :order
 end
