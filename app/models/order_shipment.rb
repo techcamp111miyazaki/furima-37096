5a18f8fab187911faae65c681c60e23152815ed9
techcamp111miyazaki <kirohi19980715@gmail.com>
2021-11-09 08:48:15 +0900

    formオブジェクトの修正

--- app/models/order_shipment.rb
+++ app/models/order_shipment.rb
@@ -8,11 +8,12 @@ class OrderShipment
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :house_number
+    validates :telephone_number
   end
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
-    order = Order.create(user_id: user_id, item_id: item_id)
-    Shipment.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, order_id: order.id)
+    order = Order.create(user_id: current_user.id, item_id: item.id)
+    Shipment.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, telephone_number: telephone_number, order_id: order.id)
   end
 end
