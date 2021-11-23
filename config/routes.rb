565d12e25121f168c8923fdec841d6b2d2066105
techcamp111miyazaki <kirohi19980715@gmail.com>
2021-11-23 18:02:20 +0900

    実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items do
+    resources :comments, only: :create
     resources :orders, only: [:index, :create]
     collection do
       get 'search'
